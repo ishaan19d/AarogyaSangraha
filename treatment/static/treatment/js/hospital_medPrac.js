a408202aeec2c1a51bdf83501c2de4335c0fdad6
ishaan19d <ishaandas848@gmail.com>
2024-04-14 23:07:37 +0530

    fixed the medPrac list filtering/ordering

--- treatment/static/treatment/js/hospital_medPrac.js
+++ treatment/static/treatment/js/hospital_medPrac.js
@@ -19,3 +19,74 @@ function filterAndSearchFunction() {
     }       
   }
 }
+
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("doctorTable");
+  switching = true;
+  dir = "asc";
+
+  while (switching) {
+    switching = false;
+    rows = table.rows;
+
+    for (i = 1; i < (rows.length - 1); i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+
+      // Check if both cells have numeric values
+      if (!isNaN(parseFloat(x.innerHTML)) && !isNaN(parseFloat(y.innerHTML))) {
+        if (dir == "asc") {
+          if (parseFloat(x.innerHTML) > parseFloat(y.innerHTML)) {
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (parseFloat(x.innerHTML) < parseFloat(y.innerHTML)) {
+            shouldSwitch = true;
+            break;
+          }
+        }
+      } else {
+        // If one or both cells have "N/A", handle the sorting
+        if (dir == "asc") {
+          if (x.innerHTML === "N/A" && y.innerHTML !== "N/A") {
+            shouldSwitch = false;
+          } else if (x.innerHTML !== "N/A" && y.innerHTML === "N/A") {
+            shouldSwitch = true;
+            break;
+          } else if (x.innerHTML === "N/A" && y.innerHTML === "N/A") {
+            shouldSwitch = false;
+          } else if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML === "N/A" && y.innerHTML !== "N/A") {
+            shouldSwitch = true;
+            break;
+          } else if (x.innerHTML !== "N/A" && y.innerHTML === "N/A") {
+            shouldSwitch = false;
+          } else if (x.innerHTML === "N/A" && y.innerHTML === "N/A") {
+            shouldSwitch = false;
+          } else if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+    }
+
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      switchcount++;
+    } else {
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
